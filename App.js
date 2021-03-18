6b086064ed4ed7b06a1834814320c404fa01338e
Caden Chabot <chabotc@myumanitoba.ca>
2021-03-18 17:42:35 -0500

    counter example to show state

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+  const [count, setCount] = useState(0);
+  const increment = () => {
+    setCount(count + 1);
+  };
+
   return (
     <View style={styles.container}>
       <Text>Home Maintenance Hub</Text>
+      <Text>{count}</Text>
+      <Button title="Increment counter" onPress={increment}></Button>
       <StatusBar style="auto" />
     </View>
   );
