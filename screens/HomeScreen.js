e5203b1b9be3a4e185bc025c8483d87977446a1d
brettlungal <lungalb@myumanitoba.ca>
2021-03-19 00:12:42 -0500

    add buttons to homescreen.js

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,7 +2,24 @@ import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 
 const HomeScreen = ({ navigation }) => {
-  return <Text>Home</Text>;
+  return (<View style={StyleSheet.Container}>
+          <Button title="Carpentry" style={StyleSheet.Button}/>
+          <Button title="Plumbling" style={StyleSheet.Button}/>
+          <Button title="Roofing" style={StyleSheet.Button}/>
+          <Button title="Electrical" style={StyleSheet.Button}/>
+          <Button title="HVAC" style={StyleSheet.Button}/>
+      </View> );
+
+  
 };
+
+const styles = StyleSheet.create({
+  Container: {
+
+  },
+  Button:{
+    backgroundColor: "#2196F3"
+  }
+});
 
 export default HomeScreen;
