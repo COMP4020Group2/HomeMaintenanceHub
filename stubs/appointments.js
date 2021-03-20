35ab879671b2119d13c90c4e9198f9e641fc7c06
Caden Chabot <chabotc@myumanitoba.ca>
2021-03-20 13:57:15 -0500

    created appointment cards and updated stub a bit

--- stubs/appointments.js
+++ stubs/appointments.js
@@ -1,7 +1,29 @@
-let appointments = [{}];
+let appointments = [
+  {
+    id: 1,
+    name: "Brett's Premium Carpentry",
+    date: 'July 9, 2021',
+    time: '12:00 PM',
+    description: 'Quote'
+  },
+  {
+    id: 2,
+    name: "Brett's Mediocre Carpentry",
+    date: 'June 18, 2021',
+    time: '12:00 PM',
+    description: 'Quote'
+  },
+  {
+    id: 3,
+    name: "Brett's Budget Carpentry",
+    date: 'April 1, 2021',
+    time: '12:00 PM',
+    description: 'Quote'
+  }
+];
 
 module.exports = {
   getAllAppointments() {
     return appointments;
-  },
+  }
 };
