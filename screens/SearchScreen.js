746187aceda87d6e99778d1f236fdc131d4c2cbe
brogrammer39 <lungalb@myumanitoba.ca>
2021-03-19 20:42:04 -0500

    bretts screen

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -2,18 +2,23 @@ import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 
 const SearchScreen = ({ navigation }) => {
-  return (
-    <View style={styles.Container}>
-      <Text>Home Maintenance Hub</Text>
-    </View>
-  );
+  return (<View style={styles.Container}>
+    <Button title="Carpentry" style={styles.Button}/>
+    <Button title="Plumbling" style={styles.Button}/>
+    <Button title="Roofing" style={styles.Button}/>
+    <Button title="Electrical" style={styles.Button}/>
+    <Button title="HVAC" style={styles.Button}/>
+</View> );
 };
 
 const styles = StyleSheet.create({
   Container: {
-    flex: 1,
+    flex: 2,
     justifyContent: 'center',
     alignItems: 'center'
+  },
+  Button:{
+    width:100
   }
 });
 
