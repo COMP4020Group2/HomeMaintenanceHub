651959a1d87fa8c6266938f29431aae889df5edc
Caden Chabot <chabotc@myumanitoba.ca>
2021-03-20 21:54:17 -0500

    Update HomeScreen.js

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,23 +2,21 @@ import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 
 const HomeScreen = ({ navigation }) => {
-  return (<View style={StyleSheet.Container}>
-          <Button title="Carpentry" style={StyleSheet.Button}/>
-          <Button title="Plumbling" style={StyleSheet.Button}/>
-          <Button title="Roofing" style={StyleSheet.Button}/>
-          <Button title="Electrical" style={StyleSheet.Button}/>
-          <Button title="HVAC" style={StyleSheet.Button}/>
-      </View> );
-
-  
+  return (
+    <View style={StyleSheet.Container}>
+      <Button title="Carpentry" style={StyleSheet.Button} />
+      <Button title="Plumbling" style={StyleSheet.Button} />
+      <Button title="Roofing" style={StyleSheet.Button} />
+      <Button title="Electrical" style={StyleSheet.Button} />
+      <Button title="HVAC" style={StyleSheet.Button} />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-  Container: {
-
-  },
-  Button:{
-    backgroundColor: "#2196F3"
+  Container: {},
+  Button: {
+    backgroundColor: '#2196F3'
   }
 });
 
