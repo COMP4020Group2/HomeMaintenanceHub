01ea931d3c5e56f0a61059f7338c9718a6d8b24b
Jordan Portz <portzj@myumanitoba.ca>
2021-03-21 18:04:54 -0500

    rip

--- screens/BookScreen.js
+++ screens/BookScreen.js
@@ -1,8 +1,69 @@
-import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import React, {useState} from 'react';
+import {View, TouchableOpacity} from 'react-native';
+import {Agenda} from 'react-native-calendars';
+import {Card, Avatar, Text} from 'react-native-paper';
+
+const timeToString = (time) => {
+  const date = new Date(time);
+  return date.toISOString().split('T')[0];
+};
 
 const BookScreen = ({ navigation }) => {
-  return <Text>Booking Screen</Text>;
+  const [items, setItems] = useState({});
+
+  const loadItems = (day) => {
+    setTimeout(() => {
+      for (let i = 0; i < 90; i++) {
+        const time = day.timestamp + i * 24 * 60 * 60 * 1000;
+        const strTime = timeToString(time);
+        if (!items[strTime]) {
+          items[strTime] = [];
+          const numItems = 2;
+          for (let j = 0; j < numItems; j++) {
+            items[strTime].push({
+              name: 'Item for ' + strTime + ' #' + j,
+              height: 100,
+            });
+          }
+        }
+      }
+      const newItems = {};
+      Object.keys(items).forEach((key) => {
+        newItems[key] = items[key];
+      });
+      setItems(newItems);
+    }, 1000);
+  };
+
+  const renderItem = (item) => {
+    return (
+      <TouchableOpacity style={{marginRight: 10, marginTop: 17}}>
+        <Card>
+          <Card.Content>
+            <View
+              style={{
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                alignItems: 'center',
+              }}>
+              <Text>{item.name}</Text>
+            </View>
+          </Card.Content>
+        </Card>
+      </TouchableOpacity>
+    );
+  };
+
+  return (
+    <View style={{flex: 1}}>
+      <Agenda
+        items={items}
+        loadItemsForMonth={loadItems}
+        selected={new Date()}
+        renderItem={renderItem}
+      />
+    </View>
+  );
 };
 
 export default BookScreen;
