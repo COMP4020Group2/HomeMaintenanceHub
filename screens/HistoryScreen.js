b8534786a30447d8014b7a73b00baa6573c0d00f
Caden Chabot <chabotc@myumanitoba.ca>
2021-03-20 22:39:01 -0500

    yeeeeeeeeeeeeet

--- screens/HistoryScreen.js
+++ screens/HistoryScreen.js
@@ -1,20 +1,26 @@
-import React, {useState} from 'react';
-import { StyleSheet, Text, View} from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View } from 'react-native';
 import HistoryCard from '../components/HistoryCard';
-import {getAllPastOrders} from '../stubs/pastorders';
-import {Picker} from '@react-native-picker/picker';
+import { getAllPastOrders } from '../stubs/pastorders';
+import { Picker } from '@react-native-picker/picker';
+import { useEffect } from 'react';
+import { useIsFocused } from '@react-navigation/native';
 
 const HistoryScreen = ({ navigation }) => {
   const [selectedDate, setSelectedDate] = useState();
-  const orders = getAllPastOrders();
+  const isFocused = useIsFocused();
+  const [orders, setOrders] = useState();
+  useEffect(() => {
+    setOrders(getAllPastOrders());
+  }, [isFocused]);
+
   return (
     <View style={styles.container}>
       <View style={styles.dropMenu}>
         <Picker
           selectedValue={selectedDate}
-          onValueChange={(itemValue, itemIndex) =>
-            setSelectedDate(itemValue)
-          }>
+          onValueChange={(itemValue, itemIndex) => setSelectedDate(itemValue)}
+        >
           <Picker.Item label="All" value="All" />
           <Picker.Item label="Past Year" value="pastYear" />
           <Picker.Item label="Past 6 Months" value="past6Months" />
@@ -23,8 +29,12 @@ const HistoryScreen = ({ navigation }) => {
         </Picker>
       </View>
       <View style={styles.historyEntries}>
-        {orders.map((history, index) => (
-          <HistoryCard key={index} history={history} navigate={navigation.navigate} />
+        {orders?.map((history, index) => (
+          <HistoryCard
+            key={index}
+            history={history}
+            navigate={navigation.navigate}
+          />
         ))}
       </View>
     </View>
@@ -32,14 +42,14 @@ const HistoryScreen = ({ navigation }) => {
 };
 
 const styles = StyleSheet.create({
-  container:{
-    marginTop:"10%"
+  container: {
+    marginTop: '10%'
   },
-  historyEntries:{
-    height:"89%"
+  historyEntries: {
+    height: '89%'
   },
-  dropMenu:{
-    height:"11%"
+  dropMenu: {
+    height: '11%'
   }
 });
 
