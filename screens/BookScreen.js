42c5be4b3973ee03536fccebbd0158ec17cd28f3
Jordan Portz <portzj@myumanitoba.ca>
2021-03-21 20:15:07 -0500

    calendar working except for booking

--- screens/BookScreen.js
+++ screens/BookScreen.js
@@ -1,44 +1,89 @@
 import React, {useState} from 'react';
 import {View, TouchableOpacity} from 'react-native';
 import {Agenda} from 'react-native-calendars';
-import {Card, Avatar, Text} from 'react-native-paper';
+import {Card, Text, Portal, Dialog, Button} from 'react-native-paper';
+import { getAvailabilities } from '../stubs/availabilities'
 
-const timeToString = (time) => {
-  const date = new Date(time);
-  return date.toISOString().split('T')[0];
+const getMonday = (day) => {
+  const date = new Date(day.timestamp);
+  let d = date.getDay();
+  if (d === 6) { d = -1; }
+  date.setDate(date.getDate() - d);
+
+  return date;
 };
 
 const BookScreen = ({ navigation }) => {
   const [items, setItems] = useState({});
+  const [dialogVisible, setDialogVisible] = useState(false);
+  const [time, setTime] = useState("");
+  const [type, setType] = useState("");
+  const [day, setDay] = useState("");
+
+  const showDialog = (time, type) => {
+    setTime(time);
+    setType(type);
+    setDialogVisible(true);
+  }
+
+  const hideDialog = () => setDialogVisible(false);
 
   const loadItems = (day) => {
-    setTimeout(() => {
-      for (let i = 0; i < 90; i++) {
-        const time = day.timestamp + i * 24 * 60 * 60 * 1000;
-        const strTime = timeToString(time);
-        if (!items[strTime]) {
-          items[strTime] = [];
-          const numItems = 2;
-          for (let j = 0; j < numItems; j++) {
-            items[strTime].push({
-              name: 'Item for ' + strTime + ' #' + j,
-              height: 100,
-            });
-          }
-        }
-      }
-      const newItems = {};
-      Object.keys(items).forEach((key) => {
-        newItems[key] = items[key];
-      });
-      setItems(newItems);
-    }, 1000);
+    setItems(getAvailabilities(getMonday(day)));
+  };
+
+  const formatDate = (day) => {
+    let month = "";
+    switch(day.month) {
+      case 1:
+        month = 'January'
+        break;
+      case 2:
+        month = 'February'
+        break;
+      case 3:
+        month = 'March'
+        break;
+      case 4:
+        month = 'April'
+        break;
+      case 5:
+        month = 'May'
+        break;
+      case 6:
+        month = 'June'
+        break;
+      case 7:
+        month = 'July'
+        break;
+      case 8:
+        month = 'August'
+        break;
+      case 9:
+        month = 'September'
+        break;
+      case 10:
+        month = 'October'
+        break;
+      case 11:
+        month = 'November'
+        break;
+      case 12:
+        month = 'December'
+        break;
+    }
+    return `${month} ${day.day}, ${day.year}`;
+  }
+
+  const bookAppointment = () => {
+    // TODO: add to appointment
+    hideDialog();
   };
 
   const renderItem = (item) => {
     return (
-      <TouchableOpacity style={{marginRight: 10, marginTop: 17}}>
-        <Card>
+      <TouchableOpacity style={{marginRight: 10, marginTop: 30, height: 60}} onPress={() => showDialog(item.time, item.type)}>
+        <Card style={{height: '100%'}}>
           <Card.Content>
             <View
               style={{
@@ -46,7 +91,8 @@ const BookScreen = ({ navigation }) => {
                 justifyContent: 'space-between',
                 alignItems: 'center',
               }}>
-              <Text>{item.name}</Text>
+              <Text>{item.time}</Text>
+              <Text>{item.type}</Text>
             </View>
           </Card.Content>
         </Card>
@@ -57,11 +103,26 @@ const BookScreen = ({ navigation }) => {
   return (
     <View style={{flex: 1}}>
       <Agenda
+        minDate={new Date()}
         items={items}
         loadItemsForMonth={loadItems}
         selected={new Date()}
         renderItem={renderItem}
+        onDayPress={(day) => {setDay(formatDate(day))}}
       />
+      <Portal>
+        <Dialog visible={dialogVisible} onDismiss={hideDialog}>
+          <Dialog.Title>Confirm</Dialog.Title>
+          <Dialog.Content>
+            <Text>Book Appointment for {type} on:</Text>
+            <Text>{day} at {time}?</Text>
+          </Dialog.Content>
+          <Dialog.Actions>
+            <Button onPress={hideDialog}>Cancel</Button>
+            <Button onPress={bookAppointment}>Ok</Button>
+          </Dialog.Actions>
+        </Dialog>
+      </Portal>
     </View>
   );
 };
